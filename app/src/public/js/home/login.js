46f2210e82bd5daed8c4f791ae45c72403f4fe7e
ng <109261336+hojo889@users.noreply.github.com>
2023-03-05 15:45:55 +0900

    fetch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,6 +10,14 @@ function login(){
     const req ={
       id: id.value,
       psw: psw.value,
-    };
-    console.log(req);
+    };//오브젝트 형태로 설정
+    
+    // fetch(경로, value);
+    fetch("/login", {
+        mathod: "POST",
+        headers:{
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify(req),
+    });
 }
