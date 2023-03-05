78c794153a171dbea30005c2164235dd857199f5
ng <109261336+hojo889@users.noreply.github.com>
2023-03-05 15:41:05 +0900

    DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("bye")
+const id = document.querySelector("#id"),
+     psw = document.querySelector('#psw'),
+     loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req ={
+      id: id.value,
+      psw: psw.value,
+    };
+    console.log(req);
+}
